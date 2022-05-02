55e617cf7dfa11fb59a38853473fc0b110a12cdd
Eric Triebe <eric.triebe@gmail.com>
2022-05-02 10:29:28 -0700

    Update module. file

--- scripts/module.js
+++ scripts/module.js
@@ -5,3 +5,17 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+Hooks.on("renderSidebarTab",(settings) => {
+	if(!game.user.isGM || settings.id != "combat") return
+	const html = settings.element
+	if(html.find("#combatEstimateButton").length !== 0) return
+	const button = `<button id="combatEstimateButton" style="flex-basis: auto;">
+	<i class="fas fa-calculator"></i> Combat Estimate
+	</button>`
+	html.find(`.header-actions`).first().append(button)
+	html.find("#combatEstimateButton").on("click",async (e) => {
+		e.preventDefault();
+		// if (!canvas.sfDialog?.rendered) await canvas.sfDialog.render(true);
+	})
+  });
