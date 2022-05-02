04035b332ba9a2926b3d0534e7c1a11070d91039
Eric Triebe <eric.triebe@gmail.com>
2022-05-02 13:27:53 -0700

    Add list of players to dialog

--- scripts/lib/dialog.js
+++ scripts/lib/dialog.js
@@ -19,6 +19,54 @@ constructor() {
 	async activateListeners(html) {
 		super.activateListeners(html);
 		const _this=this;
+		this.populateCombatants();
+	}
+
+	populateCombatants()
+	{
+		const html = this.element;
+		let $friendsList = html.find('#friendly-combatants ul').first();
+		let $hostilesList = html.find('#hostile-combatants ul').first();
+		if (!game.combat.isActive)
+		{
+			console.log(`No combat active.`)
+			return;
+		}
+
+		let combatants = game.combat.combatants;
+
+		for (let combatant of combatants)
+		{
+			let combatantDisposition = CEFoundryUtils.getCombatantDisposition(combatant);
+
+			// Disposition is 1 for friendly, 0 for neutral, -1 for hostile
+			switch (combatantDisposition)
+			{
+				case CONST.TOKEN_DISPOSITIONS.FRIENDLY:
+					console.log(`Combatant ${combatant.name} is friendly`);
+					$friendsList.append(`<li class="combatant-friendly">
+							<div class="player-details">
+								${combatant.name}
+							</div>
+						</li>`);
+					break;
+				case CONST.TOKEN_DISPOSITIONS.NEUTRAL:
+					console.log(`Combatant ${combatant.name} is neutral`);
+					break;
+					case CONST.TOKEN_DISPOSITIONS.HOSTILE:
+					console.log(`Combatant ${combatant.name} is hostile`);
+					
+					$hostilesList.append(`<li class="combatant-hostile">
+							<div class="player-details">
+								${combatant.name}
+							</div>
+						</li>`);
+					break;
+				default:
+					console.log(`Combatant ${combatant.name} has unexpected state.`);
+					break;
+			}
+		}
 	}
 }
 
--- scripts/module.js
+++ scripts/module.js
@@ -7,15 +7,22 @@ Hooks.once('ready', async function() {
 });
 
 Hooks.on("renderSidebarTab",(settings) => {
-	if(!game.user.isGM || settings.id != "combat") return
+	if(!game.user.isGM || settings.id != "combat")
+	{
+		return;
+	}
 	const html = settings.element
-	if(html.find("#combatEstimateButton").length !== 0) return
+	if(html.find("#combatEstimateButton").length !== 0)
+	{
+		return;
+	}
+
 	const button = `<button id="combatEstimateButton" style="flex-basis: auto;">
 	<i class="fas fa-calculator"></i> Combat Estimate
-	</button>`
-	html.find(`#combat-round`).first().append(button)
+	</button>`;
+	html.find(`#combat-round`).first().append(button);
 	html.find("#combatEstimateButton").on("click",async (e) => {
 		e.preventDefault();
 		if (!canvas.CombatEstimateDialog?.rendered) await canvas.CombatEstimateDialog.render(true);
-	})
+	});
 });
